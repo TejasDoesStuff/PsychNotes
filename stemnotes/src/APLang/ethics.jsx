18ff95223674c606ef6b7fe2c4bf553681b3cb70
TejasDoesStuff <tejaspanja@gmail.com>
2025-01-20 21:35:58 -0800

    i forgor what this is

---
+++ stemnotes/src/APLang/ethics.jsx
@@ -0,0 +1,38 @@
+export default function people() {
+  return (
+    <div className="w-full h-full p-12">
+      <h1 className="text-2xl font-bold underline">Ethical Perspectives</h1>
+
+      <h2 className="text-xl font-bold">List of perspectives to know</h2>
+      <p>
+        this is a brief overview of each person we went over in class and their
+        beliefs with the reading.
+      </p>
+
+      <div>
+        <div className="w-full p-4 border-b border-t border-gray-200">
+          <h1 className="font-bold">Egoism</h1>
+          <div>
+            Egoism is when you only do actions that benefit you. It is
+            the belief that people are motivated by self-interest.
+            <br />
+            <h2 className="font-bold">People</h2>
+            <li>Glaucon</li>
+          </div>
+        </div>
+        <div className="w-full p-4 border-b border-gray-200">Peter Singer</div>
+        <div className="w-full p-4 border-b border-gray-200">Ruth Benedict</div>
+        <div className="w-full p-4 border-b border-gray-200">Louis Pojman</div>
+        <div className="w-full p-4 border-b border-gray-200">Sam Harris</div>
+        <div className="w-full p-4 border-b border-gray-200">
+          John Stuart Mill
+        </div>
+        <div className="w-full p-4 border-b border-gray-200">Kant</div>
+        <div className="w-full p-4 border-b border-gray-200">
+          Bertrand Russel
+        </div>
+        <div className="w-full p-4 border-b border-gray-200">Richard Rorty</div>
+      </div>
+    </div>
+  );
+}
--- stemnotes/src/APLang/people.jsx
+++ stemnotes/src/APLang/people.jsx
@@ -1,30 +1,140 @@
 export default function people() {
-    return (
-        <div className="w-full h-full p-12">
-            <h1 className="text-2xl font-bold underline">People</h1>
+  return (
+    <div className="w-full h-full p-12">
+      <h1 className="text-2xl font-bold underline">People</h1>
 
-            <h2 className="text-xl font-bold">List of people to know</h2>
-            <p>this is a brief overview of each person we went over in class and their beliefs with the reading.</p>
-            
-            <div>
-                <h3 className="text-lg font-semibold">Philosophers</h3>
-                <div className="w-full p-4 border-b border-gray-200">Plato</div>
-                <div className="w-full p-4 border-b border-gray-200">Peter Singer</div>
-                <div className="w-full p-4 border-b border-gray-200">Ruth Benedict</div>
-                <div className="w-full p-4 border-b border-gray-200">Louis Pojman</div>
-                <div className="w-full p-4 border-b border-gray-200">Sam Harris</div>
-                <div className="w-full p-4 border-b border-gray-200">John Stuart Mill</div>
-                <div className="w-full p-4 border-b border-gray-200">Kant</div>
-                <div className="w-full p-4 border-b border-gray-200">Bertrand Russel</div>
-                <div className="w-full p-4 border-b border-gray-200">Richard Rorty</div>
-            </div>
+      <h2 className="text-xl font-bold">List of people to know</h2>
+      <p>
+        this is a brief overview of each person we went over in class and their
+        beliefs with the reading.
+      </p>
 
-            <div>
-                <h3 className="text-lg font-semibold">Authors</h3>
-                <div className="w-full p-4 border-b border-gray-200">Tim O'Brien</div>
-                <div className="w-full p-4 border-b border-gray-200">Viet Thanh Nguyen</div>
-                <div className="w-full p-4 border-b border-gray-200">James Lowen</div>
-            </div>
-        </div>  
-    )
+      <div className="mt-8">
+        <h3 className="text-lg font-semibold">Philosophers</h3>
+        <div className="w-full p-8 border-b border-t border-gray-200">
+          <span className="font-bold">Plato (Socrates)</span> - Allegory of the
+          Cave, The Ring of Gyges
+          <div className="p-2 px-10">
+            <h2 className="font-bold">Allegory of the Cave:</h2>
+            <p className="px-8 w-3/4 h-auto">
+              This story describes a group of people who have spent their entire
+              lives chained to rocks inside of a cave. The only thing they can
+              see is the wall in front of them. Behind them is a fire and
+              between the fire and the people is a walkway where people walk by
+              holding objects. The people in the cave can only see the shadows
+              of the objects on the wall in front of them. One day, one of the
+              people is freed and sees the objects and the fire. He is then
+              taken outside of the cave and sees the sun and the world for the
+              first time. He then goes back into the cave to tell the others
+              about the world outside of the cave. The people in the cave do not
+              believe him and think he is crazy.
+              <br />
+              <br />
+              The shadows are the prisoners 'reality', even though they are not
+              accurate to the real world. The shadows represent the 'false'
+              reality that we see through our senses, while the lit up objects
+              represent the true reality that people cannot see.
+              <br />
+              <br />
+              Plato uses this story to show that people are inherently biased
+              and they percieve things based off how they grew up and stuff
+            </p>
+          </div>
+          <div className="p-2 px-10">
+            <h2 className="font-bold">The Ring of Gyges:</h2>
+            <p className="px-8 w-3/4 h-auto">
+              Glaucon and Socrates are having a discussion about justice.
+              <br />
+              <br />
+              The reading begins with a discussion between Socrates and Glaucon
+              about justice. Glaucon describes 3 types of good:
+              <li>good that we do because we like the outcome</li>
+              <li>
+                good that we do because we like doing it and we like what the
+                outcome is
+              </li>
+              <li>
+                good that we do because we like doing it, regardless of the
+                outcome
+              </li>
+              Socrates agrees with option 2 while Glaucon agrees with option 3.
+              <br />
+              <br />
+              This reading is about a shepherd who found a ring on the corpse of
+              a guy inside a bronze horse. He takes the ring and finds out that
+              it can turn him invisible/visible to those around him. After
+              discovering this power, he went as a kings messanger, cheated on
+              the king with his wife, and the two of them planned and killed the
+              king. The shepherd then ruled the land with the kings wife.
+              <br />
+              <br />
+              Glaucon uses this story to show that even just men, when given the
+              power to do so, will turn against justice, proving that people are
+              not 'willingly just' and immideately become unjust when they have
+              no consequences to their actions.
+              <br />
+              <br />
+              Socrates disagrees with Glaucon and says that people are just for
+              the sake of being just, not because they are afraid of the
+              consequences.
+            </p>
+          </div>
+          <div>
+            <h2 className="font-bold">
+              Plato/Socrates believed that you should live a virtuious life
+              where you should think for yourself.
+            </h2>
+          </div>
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          <span className="font-bold">Peter Singer</span> - “Why Act Morally?” -
+          Utilitarianism
+          <div className="p-2 px-10">
+            <h2 className="font-bold">Why act morally?:</h2>
+            <p className="px-8 w-3/4 h-auto">
+              Signer argues that people should act morally because it is the most beneficial way to help create an enviornment that benefits everyone.
+              <br />
+              <br />
+              
+            </p>
+          </div>
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          Ruth Benedict - “A Defense of Moral Relativism” - Cultural Relativism
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          Louis Pojman - Moral Objectivism
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          Sam Harris - Science-based ethics
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          John Stuart Mill - Utilitarianism
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          Kant - Kantian Ethics
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          Bertrand Russel - "Appearance and Reality" - Utilitarianism
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          Richard Rorty - “Relativism: Finding and Making” - Relativism
+        </div>
+        <div>Aristotle - Virtue Ethics</div>
+      </div>
+
+      <div className="mt-8">
+        <h3 className="text-lg font-semibold">Authors</h3>
+        <div className="w-full p-8 border-b border-gray-200">
+          Tim O'Brien - "The Things They Carried"
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          Viet Thanh Nguyen - "On True War Stories"
+        </div>
+        <div className="w-full p-8 border-b border-gray-200">
+          James Lowen - “See No Evil,” from Lies My Teacher Told Me
+        </div>
+      </div>
+    </div>
+  );
 }
