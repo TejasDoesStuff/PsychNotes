e5f27bbe25f9ecaf9b78084af84e69186d040c39
TejasDoesStuff <tejaspanja@gmail.com>
2025-01-23 23:00:34 -0800

    something for today lmao

--- stemnotes/src/APPsych/unit2.jsx
+++ stemnotes/src/APPsych/unit2.jsx
@@ -3,6 +3,12 @@ export default function unit2() {
         <div className="w-full h-full p-12">
             <h1 className="text-2xl">Social Psychology</h1>
             
+            <h2>Fundamental Attribution Error</h2>
+            <p>When we overestimate the impact of dispositional influences on other people's behavior.</p>
+            <p>Ex: When someone cuts you off in traffic, you think they are a bad driver.</p>
+            <p>When we underestimate the impact of situational influences on other people's behavior.</p>
+            <p>Ex: When you cut someone off in traffic, you think you are in a hurry.</p>
+            <br />
         </div>  
     )
 }
